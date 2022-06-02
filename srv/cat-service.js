0261f3d69481f18a980d3f444cfa7b42decc51fa
Vijay Singh Rajput <vijay.singh.rajput@sap.com>
2022-06-02 13:55:23 +0000

    function implementation

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,8 +1,17 @@
 const cds = require('@sap/cds')
 module.exports = async function(){ 
-
+    const {Books, Authors} = this.entities;
     this.after ('READ','Books', each => {
         if (each.availiableStock > 111) each.title += ` -- 11% discount!`
       })
+
+    this.on ('health', () => true)  
+
+    this.on('getStock', Books, async req => {
+        console.log(req.query);
+        const result = await cds.run(req.query);
+        console.log(result[0].availiableStock);
+        return result[0].availiableStock;
+    })
 
 }
